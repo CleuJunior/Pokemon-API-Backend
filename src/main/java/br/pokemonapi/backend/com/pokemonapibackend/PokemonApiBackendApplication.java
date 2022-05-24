642212311d4a6dc59bf08231af79028f5151b440
Cleonildo Jr <cleu.junior@gmail.com>
2022-05-24 13:32:51 -0300

    Initial Porject

---
+++ src/main/java/br/pokemonapi/backend/com/pokemonapibackend/PokemonApiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.pokemonapi.backend.com.pokemonapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonApiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/pokemonapi/backend/com/pokemonapibackend/PokemonApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pokemonapi.backend.com.pokemonapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
